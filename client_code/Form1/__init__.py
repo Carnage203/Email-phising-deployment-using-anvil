f0c2bf5ab1257c72a9bff52357f1ea4b4928e94e
sohamkvmandal@gmail.com <sohamkvmandal@gmail.com>
2024-04-27 05:17:53 +0000

    Created Mail_Phising.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,3 +8,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  
+  def Check_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    Mail=anvil.server.call('Check',
+                        self.Link.text)
+    if Mail=="true":
+      self.Mail.visible=True
+      self.Mail.text="Not Safe"
+    else:
+      self.Mail.visible=True
+      self.Mail.text="Safe"
